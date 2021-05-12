d47adcf04b78945ba52f9589003e7c2a376d76ef
mahlukat5 <temmuz.sanaga@hotmail.com>
2021-05-12 03:30:40 +0300

    Update guiCreateGridList.lua

--- guiCreate/guiCreateGridList.lua
+++ guiCreate/guiCreateGridList.lua
@@ -1,41 +1,22 @@
-function guiCreateGridList(x,y,g,u,relative,parent,kenarrenk)
+function guiCreateGridList(x,y,g,u,relative,parent)
 	Lsayi = #Ltablo +1
 	if not Ltablo[Lsayi] then Ltablo[Lsayi] = {} end
-	local L = Ltablo[Lsayi]
-	
-	if not kenarrenk or string.len(kenarrenk) > 6 then
-		kenarrenk =  "1883D7"
-	end
-		
+	local L = Ltablo[Lsayi]	
 	if relative  then
-		if parent then px,pu = guiGetSize(parent,false) end
-		if not parent then px,pu = guiGetScreenSize() end
-		x,y,g,u = x*px,y*pu,g*px,u*pu
+		px,pu=getParentSize(parent)
+		x,y,g,u=x*px,y*pu,g*px,u*pu
 	end
-	
 	L.resim = guiCreateLabel(x,y,g,u, "", false, parent)
 	L.liste = _guiCreateGridList(-8,-10,g+20, u+18,false, L.resim)
-	
 	L.kenarlar = {
-		ortaUst = guiCreateStaticImage(0,0,g,1,resimOlustur("test"), false, L.resim),
-		ortaAlt = guiCreateStaticImage(0,u-1,g,1,resimOlustur("test"), false, L.resim),
-		sol = guiCreateStaticImage(0,0,1,u,resimOlustur("test"), false, L.resim),
-		sag = guiCreateStaticImage(g-1,0,1,u,resimOlustur("test"), false, L.resim),
+		ortaUst = createSideLine(0,0,g,1,L.resim,settings.gridlist.side_lines),
+		ortaAlt = createSideLine(0,u-1,g,1,L.resim,settings.gridlist.side_lines),
+		sol = createSideLine(0,0,1,u,L.resim,settings.gridlist.side_lines),
+		sag = createSideLine(g-1,0,1,u,L.resim,settings.gridlist.side_lines),
 	}
-
-	for i,v in pairs(L.kenarlar) do
-		renkVer(v,kenarrenk)
-		guiSetProperty(v, "AlwaysOnTop", "True")
-		guiSetAlpha(v, 0.4)
-	end	
-	
 	if not scriptler[sourceResource] then scriptler[sourceResource] = {} end
 	if not scriptler[sourceResource]["L"] then scriptler[sourceResource]["L"] = {} end
 	table.insert(scriptler[sourceResource]["L"], {Ltablo,Lsayi,L.resim})
-
-	genelGuiTablo[L.liste] = L.kenarlar
+	genelGuiTablo[L.liste]=L
 	return L.liste
 end
-
-
-
