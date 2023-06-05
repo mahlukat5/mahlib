0dc0e1d3e906b89848616fdc5273889bde4bef54
Alparslan Ali TÜFEKÇİ <103515832+alitfkc@users.noreply.github.com>
2023-06-05 19:03:45 +0300

    Update ayarlar.lua

--- ayarlar.lua
+++ ayarlar.lua
@@ -1,4 +1,4 @@
-﻿settings = {
+settings = {
 	window = {
 		back_topleft="000000",
 		back_topright="000000",
@@ -8,39 +8,39 @@
 		header_topright="000000",
 		header_bottomleft="000000",
 		header_bottomright="000000",
-		side_lines = "660099",
-		close_back = "660099",
+		side_lines = "ff6f00",
+		close_back = "ff6f00",
 	},
 	button = {
 		back_topleft="131314",
 		back_topright="131314",
 		back_bottomleft="131314",
 		back_bottomright="131314",
-		side_topleft="660099",
+		side_topleft="ff6f00",
 		-- side_topright="86d472",
-		side_topright="660099",
-		side_bottomleft="660099",
-		side_bottomright="660099",
-		side_lines = "660099",
+		side_topright="ff6f00",
+		side_bottomleft="ff6f00",
+		side_bottomright="ff6f00",
+		side_lines = "ff6f00",
 	},
 	edit = {
-		side_lines = "660099",
+		side_lines = "ff6f00",
 	},
 	gridlist = {
-		side_lines = "660099",
+		side_lines = "ff6f00",
 	},
 	memo = {
-		side_lines = "660099",
+		side_lines = "ff6f00",
 	},
 	tabpanel = {
-		side_lines = "660099",
+		side_lines = "ff6f00",
 	},
 	guilist = {
-		back_topleft="555555",
-		back_topright="555555",
-		back_bottomleft="555555",
-		back_bottomright="555555",
-		side_lines = "660099",
+		back_topleft="000000",
+		back_topright="000000",
+		back_bottomleft="000000",
+		back_bottomright="000000",
+		side_lines = "ff6f00",
 	},
 
 
